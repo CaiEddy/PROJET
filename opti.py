04042866657c7951e427fd7b16bb9d06d0c17dac
Eddy Cai <eddy_cai@hotmail.fr>
2018-03-05 16:21:55 +0100

    MAJ

--- opti.py
+++ opti.py
@@ -4,14 +4,18 @@ from soccersimulator import Strategy, SoccerAction, Vector2D
 from soccersimulator import SoccerTeam, Simulation
 from soccersimulator import show_simu
 from module.strategie import Fonceur
+from module.strategie import Fonceur_brain_Test
+from module.strategie import Fonceur_brain
 from module.strategie import RandomStrategy
 from module.strategie import Defenseur
+from module.strategie import Defenseur_2v2
 from module.strategie import Bon_joueur_1v1
 from module.strategie import Bon_joueur_2v2
 
 
-expe = ParamSearch(strategy=Fonceur(),
-                   params={'force': [1/10,1/9,1/8,1/7,1/5]},max_round_step=50)
+expe = ParamSearch(strategy=Fonceur_brain_Test(),
+                   params={'CONSTANTE_A': [2,4,6,8,10,12,14,16,18,20],
+			   'CONSTANTE_B': [25,30,35,40,45,50,55,60]},max_round_step=200)
 expe.start()
 print(expe.get_res())
 
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from soccersimulator import Strategy, SoccerAction, Vector2D
 from soccersimulator import SoccerTeam, Simulation
 from soccersimulator import show_simu
 from module.strategie import Fonceur
+from module.strategie import Fonceur_brain
 from module.strategie import RandomStrategy
 from module.strategie import Defenseur
 from module.strategie import Defenseur_2v2
@@ -11,15 +12,18 @@ from module.strategie import Bon_joueur_2v2
 
 
 ## Creation d'une equipe
-pyteam = SoccerTeam(name="VousAllezPerdre, Don't be Mad :)")
+pyteam = SoccerTeam(name="OnVaWin")
 thon = SoccerTeam(name="ThonTeam")
 
 
-pyteam.add("PyPlayer",Fonceur())
+#pyteam.add("PyPlayer",Bon_joueur_2v2())
 pyteam.add("PyPlayer",Defenseur_2v2())
 
-thon.add("ThonPlayer",Bon_joueur_2v2())
-thon.add("ThonPlayer",Defenseur_2v2())
+thon.add("ThonPlayer",Fonceur_brain())
+#thon.add("ThonPlayer",Defenseur_2v2())
+
+
+
 
 
 
