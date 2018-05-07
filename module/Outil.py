c9a081cfab8896dad6419979772a611fb780ab75
Steve Moukouri <steevemouk@gmail.com>
2018-05-07 12:36:06 +0200

    :q# Veuillez saisir le message de validation pour vos modifications. Les lignes

--- module/Outil.py
+++ module/Outil.py
@@ -5,44 +5,42 @@ from soccersimulator.settings import *
 
 
 class Outil(object):
-	def __init__(self,state,team,player,cst_dribble_A=19.5, cst_dribble_B=22, cst_defense=0.11, cst_prediction=0.6):
+	def __init__(self,state,team,player,cst_force=5,cst_prediction):
 		self.state=state
 		self.team=team
 		self.player=player
-		self.cst_dribble_A = cst_dribble_A
-		self.cst_dribble_B = cst_dribble_B
-		self.cst_defense = cst_defense
-		self.cst_prediction = cst_prediction
+		self.cst_force=cst_force
+		self.cst_prediction=cst_prediction
+###fonction de base:		
 	def goal_team(self):
 		if (self.team == 1):
 			return (0,GAME_HEIGHT/2)
 		return (GAME_WIDTH, GAME_HEIGHT/2)
-		
-	def idiot1(self):
-		return SoccerAction(self.vect_player_posi((GAME_WIDTH)*1/4,(GAME_HEIGHT)*1/2),0)
-
-	def idiot2(self):
-		return SoccerAction(self.vect_player_posi((GAME_WIDTH)*1/10,(GAME_HEIGHT)*1/2),0)
-	
-	def idiot3(self):
-		return SoccerAction(self.vect_player_posi((GAME_WIDTH)*1/15,(GAME_HEIGHT)*1/2),0)
-
-
 	def vitesse_ball(self):
-		return self.state.ball.vitesse	
+		return self.state.ball.vitesse
 	def posi_ball(self):
 		return self.state.ball.position
 	def posi_player(self):
 		return self.state.player_state(self.team,self.player).position 
+	def vitesse_player(self):
+		return self.state.player_state(self.team,self.player).vitesse
 	def dist_player_ball(self):
 		return (self.posi_ball() - self.posi_player()).norm
 	def vect_player_ball(self):
 		return (self.posi_ball() - self.posi_player())
 	def peut_tirer(self):
-		if (self.dist_player_ball() < PLAYER_RADIUS + BALL_RADIUS):
-			return True
+		me = state.player_state(1,0)
+		if ((self.vitesse_ball().norm < 00.1) && (self.vitesse_player().norm <0.1)):
+			if (me.position.distance(ball.position)<(settings.BALL_RADIUS+settings.PLAYER_RADIUS)) and  me.vitesse.norm<0.5:
+				return True
+			return False
 		return False
-		
+###Action:
+
+	def tirer_vers_la_balle:
+		if(self.peut_tirer()):
+			return SoccerAction(self.state.balls[0].posi_ball()-self.posi_ball(),cst_force)
+
 	def dist_adversaire_player(self,adversaire):
 		return (self.vect_player_posi(adversaire.x,adversaire.y).norm)
 		
@@ -50,63 +48,6 @@ class Outil(object):
 	def vect_player_posi(self,x,y):
 		return Vector2D(x,y) - self.posi_player()
 
-	def revenir_posi_def(self,id_team):
-		if (id_team == 1):
-			return SoccerAction(self.vect_player_posi(GAME_WIDTH*self.cst_defense,(((self.posi_ball().y)- (GAME_HEIGHT/2))/self.posi_ball().x)*(GAME_WIDTH*self.cst_defense) + GAME_HEIGHT/2),0)
-		return SoccerAction(self.vect_player_posi(GAME_WIDTH - (self.cst_defense*GAME_WIDTH),((((self.posi_ball().y)- (GAME_HEIGHT/2))/((self.posi_ball().x) - (GAME_WIDTH)))*(GAME_WIDTH - (self.cst_defense*GAME_WIDTH)) + GAME_HEIGHT/2 - (((self.posi_ball().y)- (GAME_HEIGHT/2))/((self.posi_ball().x)-(GAME_WIDTH)))*GAME_WIDTH)),0)
-
-	def revenir_posi_counter(self,id_team):
-		if (self.adversaire_plus_proche_derriere().y < GAME_HEIGHT/2):
-			return SoccerAction(self.vect_player_posi((GAME_WIDTH)*1/2,(GAME_HEIGHT)*3/4),0)
-		return SoccerAction(self.vect_player_posi((GAME_WIDTH)*1/2,(GAME_HEIGHT)*1/4),0)
-		
-
-
-	
-	def ball_dans_rayon_but(self, id_team):
-		if (id_team == 1):
-			return self.posi_ball().x < (GAME_WIDTH)*2/5
-		return self.posi_ball().x > (GAME_WIDTH)*3/5
-
-
-	def defendre(self, id_team):
-		return SoccerAction(self.prediction_ball(),self.renvoyer_ball(id_team))
-
-	def renvoyer_ball(self, id_team):
-		if (id_team ==1):
-			return self.vect_player_posi(GAME_WIDTH,(GAME_HEIGHT)*1/2)
-		return self.vect_player_posi(0,(GAME_HEIGHT)*1/2)
-
-
-	def bonne_precision(self,id_team):
-		if (id_team == 1):
-			return (Vector2D(GAME_WIDTH,GAME_HEIGHT/2) - self.posi_ball())/10
-		return (Vector2D(0,GAME_HEIGHT/2) - self.posi_ball())/10
-	def mauvaise_precision(self,id_team):
-		if (id_team == 1):
-			return (Vector2D(GAME_WIDTH,GAME_HEIGHT/2) - self.posi_ball())/60
-		return (Vector2D(0,GAME_HEIGHT/2) - self.posi_ball())/60
-		
-
-	def tir_bonne_precision(self,id_team):
-		return SoccerAction(self.prediction_ball(),self.bonne_precision(id_team))
-	def tir_mauvaise_precision(self,id_team):
-		return SoccerAction(self.prediction_ball(),self.mauvaise_precision(id_team))
-
-
-	def tir(self, id_team):
-		if (id_team == 1):
-			if (self.posi_ball().x > (GAME_WIDTH*3/4)):
-				return self.tir_bonne_precision(id_team)
-			return self.tir_mauvaise_precision(id_team)
-		if (id_team == 2):
-			if (self.posi_ball().x < (GAME_WIDTH*1/4)):
-				return self.tir_bonne_precision(id_team)
-			return self.tir_mauvaise_precision(id_team)
-
-
-	def courir_vers_ball_predi(self):
-		return SoccerAction(self.prediction_ball(),0)
 	
 
 	def prediction_ball(self):
@@ -115,190 +56,5 @@ class Outil(object):
 
 	def predire_la_balle(self):
 		Constante = self.cst_prediction*self.dist_player_ball()
-		return self.posi_ball() + Constante*self.vitesse_ball()
-
-
-	def rien_faire(self):
-		return SoccerAction(0,0)
-	
-	def liste_amis(self):
-		return [self.state.player_state(idteam, idplayer).position for idteam, idplayer in self.state.players if idteam == self.team and idplayer != self.id_player]
-
-	def liste_adversaire(self):
-        	return [self.state.player_state(idteam, idplayer).position for idteam, idplayer in self.state.players if idteam != self.team]
-
-
-	def devant_nous(self, player2):
-        	player1 = self.posi_player()
-        	return (player2.x > player1.x and self.team == 1) or (player2.x < player1.x and self.team == 2)
-	
-	def derriere_nous(self, player2):
-        	player1 = self.posi_player()
-        	return (player2.x < player1.x and self.team == 1) or (player2.x > player1.x and self.team == 2)
-
-	def au_dessu_nous(self, player2):
-        	player1 = self.posi_player()
-        	if player2.y > player1.y:
-        		return True
-        	return False
-	
-	def en_dessou_nous(self, player2):
-        	player1 = self.posi_player()
-        	if player2.y < player1.y:
-        		return True
-        	return False
-
-	def adversaire_devant_player(self):
-        	liste = self.liste_adversaire()
-        	for i in liste:
-            		if self.devant_nous(i):
-                		return True
-        	return False
-	
-	def adversaire_plus_proche_devant(self):
-		liste = self.liste_adversaire()
-		dist = GAME_WIDTH * 2
-		adversaire = liste[0]
-		for i in liste:
-			a = self.dist_adversaire_player(i)
-			if ((a < dist) & self.devant_nous(i)):
-				adversaire = i
-				dist = a
-		return adversaire
-	
-	def adversaire_plus_proche_derriere(self):
-		liste = self.liste_adversaire()
-		dist = GAME_WIDTH * 2
-		adversaire = liste[0]
-		for i in liste:
-			a = self.dist_adversaire_player(i)
-			if ((a < dist) & self.derriere_nous(i)):
-				adversaire = i
-				dist = a
-		return adversaire
-
-	def ami_devant_player(self):
-        	liste = self.liste_ami()
-        	for i in liste:
-            		if self.devant_nous(i):
-                		return True
-        	return False
-
-
-	def tir_vers_milieu_bas(self):
-		return SoccerAction(self.prediction_ball(), (Vector2D((GAME_WIDTH)*1/2,(GAME_HEIGHT)*1/4) - self.posi_ball())/60)
-	def tir_vers_milieu_haut(self):
-		return SoccerAction(self.prediction_ball(), (Vector2D((GAME_WIDTH)*1/2,(GAME_HEIGHT)*3/4) - self.posi_ball())/60)
-
-
-
-	def tir_vers_milieu_bas_def(self):
-		return SoccerAction(self.prediction_ball(), (Vector2D((GAME_WIDTH)*1/2,(GAME_HEIGHT)*1/4) - self.posi_ball())/8)
-	def tir_vers_milieu_haut_def(self):
-		return SoccerAction(self.prediction_ball(), (Vector2D((GAME_WIDTH)*1/2,(GAME_HEIGHT)*3/4) - self.posi_ball())/8)
-
-
-	def peut_jouer(self):
-		if (self.dist_player_ball() < 50):
-			return True
-		return False
-
-
-	def recuperation_ball_pret(self):
-		if (self.dist_player_ball() < 15):
-			return True
-		return False
-
-
-	def attaque_fonceur(self,id_team):
-		if (self.peut_tirer()):
-			return self.tir(id_team)
-		return self.courir_vers_ball_predi()
-
-
-	def defense(self,id_team):
-		if (self.ball_dans_rayon_but(id_team)):
-			if (self.peut_tirer()):
-				return self.defendre(id_team)
-			return self.courir_vers_ball_predi()
-		else:
-			return self.revenir_posi_def(id_team)
-
-			
-	def defense_2v2(self,id_team):
-		if (self.ball_dans_rayon_but(id_team)):
-			if (self.adversaire_devant_player() & self.au_dessu_nous(self.adversaire_plus_proche_devant())):
-				if (self.peut_tirer()):
-					return self.tir_vers_milieu_bas_def()
-				return self.courir_vers_ball_predi()
-			if (self.adversaire_devant_player() & self.en_dessou_nous(self.adversaire_plus_proche_devant())):
-				if (self.peut_tirer()):
-					return self.tir_vers_milieu_haut_def()
-				return self.courir_vers_ball_predi()
-			
-		return self.revenir_posi_def(id_team)
-
-
-	def dribbler_1v1(self,id_team):
-		if (self.peut_jouer()):
-			if (self.adversaire_devant_player() & self.au_dessu_nous(self.adversaire_plus_proche_devant())):
-				if (self.peut_tirer()):
-					return self.tir_vers_milieu_bas()
-				return self.courir_vers_ball_predi()
-			if (self.adversaire_devant_player() & self.en_dessou_nous(self.adversaire_plus_proche_devant())):
-				if (self.peut_tirer()):
-					return self.tir_vers_milieu_haut()
-				return self.courir_vers_ball_predi()
-			return self.attaque_fonceur(id_team)
-		return self.revenir_posi_attaquant(id_team)
-
-
-	def revenir_posi_attaquant(self,id_team):
-		if (id_team == 1):
-			return SoccerAction(self.vect_player_posi((GAME_WIDTH)*1/8,(GAME_HEIGHT)*1/2),0)
-		return SoccerAction(self.vect_player_posi((GAME_WIDTH)*7/8,(GAME_HEIGHT)*1/2),0)
-
-
-	def ball_devant_player(self,id_team):
-		if (id_team == 1):
-			if (self.posi_player().x < self.posi_ball().x):
-				return True
-			return False
-		if (self.posi_player().x > self.posi_ball().x):
-			return True
-		return False
-		
-		
-	def fonceur_brain(self,id_team):
-		if (id_team == 2):
-			if (self.adversaire_devant_player() & self.en_dessou_nous(self.adversaire_plus_proche_devant()) & (self.dist_adversaire_player(self.adversaire_plus_proche_devant()) < 40)):
-				if (self.peut_tirer()):
-					return SoccerAction(self.prediction_ball(), self.vect_player_posi(self.adversaire_plus_proche_devant().x - self.cst_dribble_A, self.adversaire_plus_proche_devant().y + self.cst_dribble_B)/60)
-				return self.courir_vers_ball_predi() 
-			if (self.adversaire_devant_player() & self.au_dessu_nous(self.adversaire_plus_proche_devant()) & (self.dist_adversaire_player(self.adversaire_plus_proche_devant()) < 40)):
-				if (self.peut_tirer()):
-					return SoccerAction(self.prediction_ball(), self.vect_player_posi(self.adversaire_plus_proche_devant().x - self.cst_dribble_A, self.adversaire_plus_proche_devant().y - self.cst_dribble_B)/60)
-				return self.courir_vers_ball_predi()
-			return self.attaque_fonceur(id_team)
-		if (self.adversaire_devant_player() & self.en_dessou_nous(self.adversaire_plus_proche_devant()) & (self.dist_adversaire_player(self.adversaire_plus_proche_devant()) < 40)):
-			if (self.peut_tirer()):
-				return SoccerAction(self.prediction_ball(), self.vect_player_posi(self.adversaire_plus_proche_devant().x + self.cst_dribble_A, self.adversaire_plus_proche_devant().y + self.cst_dribble_B)/60)
-			return self.courir_vers_ball_predi()
-
-		if (self.adversaire_devant_player() & self.au_dessu_nous(self.adversaire_plus_proche_devant()) & (self.dist_adversaire_player(self.adversaire_plus_proche_devant()) < 40)):
-			if (self.peut_tirer()):
-				return SoccerAction(self.prediction_ball(), self.vect_player_posi(self.adversaire_plus_proche_devant().x + self.cst_dribble_A, self.adversaire_plus_proche_devant().y - self.cst_dribble_B)/60)
-			return self.courir_vers_ball_predi()
-		return self.attaque_fonceur(id_team)
-
-
-
-
-
-
-
-
-
-
-
+		return self.get_collision().a + Constante*self.vitesse_ball()
 
--- test.py
+++ test.py
@@ -1,46 +1,33 @@
-from soccersimulator import Strategy, SoccerAction, Vector2D
-from soccersimulator import SoccerTeam, Simulation
-from soccersimulator import show_simu
-from module.strategie import Fonceur
-from module.strategie import Fonceur_brain
-from module.strategie import RandomStrategy
-from module.strategie import Defenseur
-from module.strategie import Defenseur_2v2
-from module.strategie import Defenseur_2v2_opti
-from module.strategie import Bon_joueur_1v1
-from module.strategie import Bon_joueur_2v2
+from soccersimulator import SoccerAction,Vector2D,settings ,SoccerTeam,Billard,show_simu,Strategy
+from module.Outil import Outil
 from module.strategie import *
+from soccersimulator import SoccerAction,Vector2D,settings ,SoccerTeam,Billard,show_simu,Strategy
+
+class FonceurLent(Strategy):
+    def __init__(self):
+        super(FonceurLent,self).__init__("fonceur")
+    def compute_strategy(self,state,idteam,idplayer):
+        ball = state.ball
+        me = state.player_state(1,0)
+        oth = state.balls[0]
+        shoot = (oth.position-ball.position)*100
+        if (me.position.distance(ball.position)<(settings.BALL_RADIUS+settings.PLAYER_RADIUS)) and  me.vitesse.norm<0.5:
+            return SoccerAction(shoot=shoot)
+        acc = ball.position-me.position
+        if acc.norm<5:
+            acc.norm=0.1
+        return SoccerAction(acceleration=acc)
+
+class tirAlea(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Random")
+    
+    def compute_strategy(self,state,id_team,id_player):
+        outil = Outil(state, id_team, id_player)
+        return outil.tirer_vers_la_balle()
 
 
-
-## Creation d'une equipe
-pyteam = SoccerTeam(name="Je suis nul et je suis devant vous -_- ???? ?PROBLEM?")
-thon = SoccerTeam(name="ThonTeam")
-
-
-#pyteam.add("PyPlayer",Bon_joueur_2v2())
-pyteam.add("PyPlayer",Defenseur_2v2())
-
-thon.add("ThonPlayer",Fonceur_brain())
-#thon.add("ThonPlayer",Defenseur_2v2())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Creation d'une partie
-simu = Simulation(pyteam,thon)
-#Jouer et afficher la partie
-show_simu(simu)
-
+myt = SoccerTeam("prof")
+myt.add("N",FonceurLent())
+b = Billard(myt,type_game=1)
+show_simu(b)
